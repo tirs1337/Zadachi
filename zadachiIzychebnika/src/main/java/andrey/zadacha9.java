9c7d86109c7dd7928324f91645b38f7aa14d1670
Andrei <165187220+tirs1337@users.noreply.github.com>
2024-04-01 13:07:20 +0300

    Add files via upload

---
+++ zadachiIzychebnika/src/main/java/andrey/zadacha9.java
@@ -0,0 +1,17 @@
+package andrey;
+
+/**
+ * Это задача со стр 63(9 номер).
+ */
+public class zadacha9 {
+    /**
+     * Метод main.
+     * @param args - аргумент метода main.
+     */
+    public static void main(String[] args) {
+        double weight;
+        weight = 0.17 * 90; // 0.17 получилось при делении 17% на 100%, а 90 взял свой вес.
+        System.out.println("Фактический вес на луне составит: " + weight);
+
+    }
+}
