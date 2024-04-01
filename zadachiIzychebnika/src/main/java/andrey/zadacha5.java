fa0a4dffc2a0de308f5ebc54350befba6e4201ce
Andrei <165187220+tirs1337@users.noreply.github.com>
2024-04-01 13:06:05 +0300

    Add files via upload

---
+++ zadachiIzychebnika/src/main/java/andrey/zadacha5.java
@@ -0,0 +1,22 @@
+package andrey.glava2;
+
+/**
+ * Тут надо было понять что не так с фрагментом кода. 5 номер, стр.93.
+ */
+
+public class zadacha5 {
+    /**
+     * метод main.
+     * @param args - аргумент метода main.
+     */
+    public static void main(String[] args) {
+        int i;
+        for(i = 0; i < 10; i++) {
+            int sum;
+            sum = ++i;
+            System.out.println("Сумма; " + sum);
+        }
+    }
+}
+// переменная i не была задана, а переменная sum была задана не правильно.
+// я дал переменную i, и после начачала цикла for, в новом блоке я задал переменную sum.
