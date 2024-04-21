011a3ba2365cd6ceeefabc75b696a1d177f69dba
AndreiM <muxala3@gmail.com>
2024-04-21 09:10:20 +0300

    Ch005 Здесь я исправил файл "pom.xml" потому что он не собирал проект, и сделал две задачи.

---
+++ zadachiIzychebnika/src/main/java/andrey/glava3/ForCode.java
@@ -0,0 +1,16 @@
+package andrey.glava3;
+
+/**
+ * Здесь я решил задачу номер 9, стр.134.
+ */
+public class ForCode {
+    /**
+     * метод main.
+     * @param args - аргумент метода main.
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Math.pow(2, i));
+            }
+        }
+    }
